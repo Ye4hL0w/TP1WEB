6ce606fa9f93a55673f4dbfc671c1ebd52816c02
Jeremy <jeremy.moncada@etu.unice.fr>
2025-01-28 14:54:05 +0100

    commit site e commerce complet 💯

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const liens = document.querySelector('.liens');
+    const lienLi = document.querySelectorAll('.liens li');
+
+    burgerMenu?.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active');
+        liens.classList.toggle('active');
+        document.body.classList.toggle('menu-open');
+    });
+
+    lienLi.forEach(item => {
+        item.addEventListener('click', () => {
+            burgerMenu.classList.remove('active');
+            liens.classList.remove('active');
+            document.body.classList.remove('menu-open');
+        });
+    });
+
+    window.addEventListener('scroll', () => {
+        if (liens.classList.contains('active')) {
+            burgerMenu.classList.remove('active');
+            liens.classList.remove('active');
+            document.body.classList.remove('menu-open');
+        }
+    });
+});
